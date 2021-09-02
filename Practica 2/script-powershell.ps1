7a066efd1d91dd408cb45317deef8f1b4a5843f6
Fidencio Vanegas <fidencio.vanegasviera@gmail.com>
2021-09-02 09:29:56 -0500

    Segundo commit

---
+++ Practica 2/script-powershell.ps1
@@ -0,0 +1,19 @@
+﻿#---------------------------funcion---------------------------#
+function tabla-mul{
+    #--------------------------ciclo--------------------------#
+    for($i = 1; $i -lt 11; $i++){
+        Write-Host $i 'x' $num '=' ($i*$num)
+    }
+}
+Write-Host "`ntabla de multiplicar de numeros positivos"
+#--------------------------variable----------------------------#
+[int]$num = [int](Read-Host -Prompt "Escribe un numero: ")
+#------------------------------if------------------------------#
+if($num -ge 0){
+    tabla-mul($num)
+}
+else{
+    Write-Host "El numero es negativo"
+}
+
+
